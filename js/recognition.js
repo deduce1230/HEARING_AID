cf7bd7de093e43dca7406e9acbed608da2821ba8
deduce1230 <48430903+deduce1230@users.noreply.github.com>
2019-10-08 11:22:07 +0900

    Update recognition.js

--- js/recognition.js
+++ js/recognition.js
@@ -1,6 +1,36 @@
-var recognition = new webkitSpeechRecognition();
-  recognition.onresult = function(event) {
-  if (event.results.length > 0) {
-    q.value = event.results[0][0].transcript;
-  }
-}
+//var recognition = new webkitSpeechRecognition();
+//  recognition.onresult = function(event) {
+//  if (event.results.length > 0) {
+//    q.value = event.results[0][0].transcript;
+//  }
+//}
+
+    var recognizing;
+    var recognition = new SpeechRecognition();
+    recognition.continuous = true;
+    reset();
+    recognition.onend = reset;
+
+    recognition.onresult = function (event) {
+      for (var i = resultIndex; i < event.results.length; ++i) {
+        if (event.results.final) {
+          textarea.value += event.results[i][0].transcript;
+        }
+      }
+    }
+
+    function reset() {
+      recognizing = false;
+      button.innerHTML = "Click to Speak";
+    }
+
+    function toggleStartStop() {
+      if (recognizing) {
+        recognition.stop();
+        reset();
+      } else {
+        recognition.start();
+        recognizing = true;
+        button.innerHTML = "Click to Stop";
+      }
+    }

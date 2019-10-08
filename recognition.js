d52c5a37b5ec0f48c182c983df80eed6c6b8b117
deduce1230 <48430903+deduce1230@users.noreply.github.com>
2019-10-08 11:13:21 +0900

    Delete recognition.js

--- recognition.js
+++
@@ -1,6 +0,0 @@
-var recognition = new webkitSpeechRecognition();
-  recognition.onresult = function(event) {
-  if (event.results.length > 0) {
-    q.value = event.results[0][0].transcript;
-  }
-}
